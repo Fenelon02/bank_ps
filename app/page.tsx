fc5139fad290e86d4ae286bf0450928c5fd6fd72
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-31 16:07:09 -0300

    FEAT: implements the hero section and the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "./components/heroSection/HeroSection";
+import Header from "./components/layout/Header";
+
 export default function Home() {
   return (
     <div>
-      
+      <Header />  
+      <HeroSection />
     </div>
   );
 }
