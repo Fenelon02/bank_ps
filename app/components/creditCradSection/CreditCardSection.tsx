63d790bb9191ad7f35c08b4f45e9fbc992414664
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-07 16:21:47 -0300

    CHORE: fix the border max width

--- app/components/creditCradSection/CreditCardSection.tsx
+++ app/components/creditCradSection/CreditCardSection.tsx
@@ -3,7 +3,7 @@ import Button from "../ui/button/Button";
 
 export default function CreditCardSection() {
     return (
-        <div className="bg-blue-800 p-3">
+        <div className="bg-blue-800 p-3 max-w-screen">
             <h2 className="text-white text-3xl font-bold mb-2 text-center mt-4">Aproveite e decole no <span className="text-green-500">Brasil</span> e no <span className="text-yellow-500">Mundo</span> com um só <span className="">Cartão</span></h2>
             <p className="text-gray-300 mb-2 text-center text-lg">Desfrute de vantagens exclusivas com nosso cartão internacional.</p>
             <CreditCardCarousel />
