d28b418aa7468f045c01dc46d66e104ab1a4de52
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 10:05:19 -0300

    FEAT: fix the button heigth

--- app/components/ui/button/buttonSizes.ts
+++ app/components/ui/button/buttonSizes.ts
@@ -1,7 +1,7 @@
 export const ButtonSizes = {
-  small: "h-8 px-3 text-sm",
-  medium: "h-10 px-4 text-base",
-  large: "h-12 px-6 text-lg",
-  xlarge: "h-14 px-8 text-xl",
+  small: "min-h-8 px-3 text-sm",
+  medium: "min-h-10 px-4 text-base",
+  large: "min-h-12 px-6 text-lg",
+  xlarge: "min-h-14 px-8 text-xl",
 }
 export type Size = keyof typeof ButtonSizes
