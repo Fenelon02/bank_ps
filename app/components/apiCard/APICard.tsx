a3b0723a35411463d088273a5b285671cf956994
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 09:51:33 -0300

    FEAT: fix the display for md screen

--- app/components/apiCard/APICard.tsx
+++ app/components/apiCard/APICard.tsx
@@ -33,9 +33,12 @@ const logos = [
 export default function APICard() {
   return(
     <div className="m-4 bg-blue-900 p-6 rounded-lg text-white grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-4 items-center">
-        <h2 className="text-3xl font-bold mb-4 text-center lg:text-5xl">API de fácil integração a <br />diferentes sistemas</h2>
-       <div className="flex justify-center">
-        {logos.map((logo, index) => (<img key={index} src={logo.logoUrl} alt={logo.name} className="h-12 lg:h-60 w-12 lg:w-40 m-2 inline-block" />))}
+        <h2 className="text-3xl font-bold mb-4 md:text-4xl text-center lg:text-5xl">API de fácil integração a <br />diferentes sistemas</h2>
+       <div className="flex justify-center lg:flex-wrap">
+        {logos.map((logo, index) => 
+          (
+            <img key={index} src={logo.logoUrl} alt={logo.name} className="h-12  md:h-25 md:w-25 lg:h-30 w-12 lg:w-30 m-2 inline-block" />
+          ))}
        </div>
     </div>
   )
