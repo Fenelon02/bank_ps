80c7acc421391b48ada4256867ea228004b6a1ee
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 16:35:34 -0300

    FEAT: update the font size on the marquee for lg screens

--- app/components/marquee/Marquee.tsx
+++ app/components/marquee/Marquee.tsx
@@ -2,8 +2,8 @@ import LogoMarquee from "./LogosMarquee";
 
 export default function MarqueeSection() { 
     return(
-        <div className="border-y border-gray-200 ">
-            <h2 className="text-2xl md:text-3xl text-blue-600 font-bold text-center mt-4">Nossos Parceiros Comerciais</h2>
+        <div className="border-y border-gray-200 lg:gap-4 lg:my-4">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl text-blue-600 font-bold text-center mt-4">Nossos Parceiros Comerciais</h2>
             <LogoMarquee />
         </div>
     )
