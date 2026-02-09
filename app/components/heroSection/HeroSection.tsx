e25eefe5e3cfeb632cdbb6c3998a2ffc316efdab
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 16:34:13 -0300

    FEAT: fix the font size on the hero section fro lg displays

--- app/components/heroSection/HeroSection.tsx
+++ app/components/heroSection/HeroSection.tsx
@@ -5,11 +5,11 @@ export default function HeroSection() {
         <div className="bg-[url(@/public/images/HeroBg.jpg)] bg-cover bg-center h-screen w-full flex items-end md:items-start">
             <section className="font-sans text-white font-bold flex flex-col p-7 md:grid md:grid-rows-2 md:h-full">
                 <div className="md:flex md:items-start md:flex-col md:mt-20 gap-2">
-                    <h2 className="text-3xl md:text-5xl">Seu Banco em sua mão</h2>
-                    <p className="text-xl md:text-2xl">Pensado em você para você.</p>    
+                    <h2 className="text-3xl md:text-5xl lg:text-6xl">Seu Banco em sua mão</h2>
+                    <p className="text-xl md:text-2xl lg:text-3xl">Pensado em você para você.</p>    
                 </div>
                 <div className="md:flex md:items-end">
-                    <Button variant="primary" size="xlarge" additionalClass="w-fit mt-2">
+                    <Button variant="primary" size="xlarge" additionalClass="mt-2 md:mt-0">
                       Criar conta
                     </Button>
                 </div>
