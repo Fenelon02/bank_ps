ac469a13c4e70604ed84295fe29a80cbaa0bfadf
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 09:54:43 -0300

    FEAT: fix the font size for md screens

--- app/components/FAQ/FAQSection.tsx
+++ app/components/FAQ/FAQSection.tsx
@@ -49,13 +49,16 @@ const bankAccountFAQ = [
 export default function FAQSection() {
     
     return (
-        <div className="mt-4 flex flex-col items-center justify-center gap-4">
+        <div className="m-4 flex flex-col items-center justify-center gap-4">
             <h2 className="text-3xl font-bold text-center text-blue-600">Dúvidas Frequentes</h2>
             {bankAccountFAQ.map((faq) => {
                 const [open, setOpen] = useState<number | null>(null);
                 return (
                     <div key={faq.id} className="w-full max-w-2xl p-2 flex flex-col items-center">
-                        <Button additionalClass={`font-semibold w-full ${open === faq.id ? "border-blue-600 border-b-transparent rounded-b-none" : "rounded-lg"}`} variant="outline" size="large" onClick={() => setOpen(open === faq.id ? null : faq.id)}>{faq.question} </Button>
+                        <Button additionalClass={`font-semibold w-full ${open === faq.id ? "border-blue-600 border-b-transparent rounded-b-none" : "rounded-lg"}`}
+                          variant="outline" size="large" onClick={() => setOpen(open === faq.id ? null : faq.id)}>
+                          {faq.question} 
+                        </Button>
                         {open === faq.id && <div className="p-4 border border-blue-600 rounded-b-lg w-full">{faq.answer}</div>}
                     </div>
                 )
