4da5bac731a3cdf3daed2a1bf2afe426513f6cbe
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 09:57:37 -0300

    FEAT: fix the font size for md screens

--- app/components/marquee/Marquee.tsx
+++ app/components/marquee/Marquee.tsx
@@ -3,7 +3,7 @@ import LogoMarquee from "./LogosMarquee";
 export default function MarqueeSection() { 
     return(
         <div className="border-y border-gray-200 ">
-            <h2 className="text-2xl text-blue-600 font-bold text-center mt-4">Nossos Parceiros Comerciais</h2>
+            <h2 className="text-2xl md:text-3xl text-blue-600 font-bold text-center mt-4">Nossos Parceiros Comerciais</h2>
             <LogoMarquee />
         </div>
     )
