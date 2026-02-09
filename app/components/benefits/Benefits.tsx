848ae6ba4d464efad9a12cb897886601a3f3c4c3
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 09:52:04 -0300

    FEAT: fix the font size for md screens

--- app/components/benefits/Benefits.tsx
+++ app/components/benefits/Benefits.tsx
@@ -3,8 +3,8 @@ import { BenefitsCarousel } from "./BenefitsCarousel";
 
 export default function Benefits() {
     return(
-        <div className="p-3 flex justify-center items-center flex-col gap-4">
-            <h2 className="text-center text-2xl font-bold text-blue-600">Aqui você faz seu dinheiro valer apena!</h2>
+        <div className="p-3 flex justify-center items-center flex-col gap-4 mt-10">
+            <h2 className="text-center text-2xl md:text-3xl font-bold text-blue-600">Aqui você faz seu dinheiro valer apena!</h2>
             <BenefitsCarousel />
         </div>
     )
