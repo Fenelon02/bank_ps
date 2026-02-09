8ab98dbbdebc0c8e41db671c758c93045015bdf8
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 09:58:27 -0300

    CHORE: update the footer path import

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { CreditCard, Factory } from "lucide-react";
 import APICard from "./components/apiCard/APICard";
 import Benefits from "./components/benefits/Benefits";
 import HeroSection from "./components/heroSection/HeroSection";
@@ -7,7 +6,7 @@ import Marquee from "./components/marquee/Marquee";
 import Offered_services from "./components/offered_services/Offered_services";
 import CreditCardSection from "./components/creditCradSection/CreditCardSection";
 import FAQSection from "./components/FAQ/FAQSection";
-import Footer from "./components/ui/footer/Footer";
+import Footer from "./components/layout/Footer";
 
 export default function Home() {
   return (
