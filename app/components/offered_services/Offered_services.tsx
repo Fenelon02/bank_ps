31291981157e82db8d98d688fa73cb07bf4727ed
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 16:36:03 -0300

    FEAT: update the font size on the ofered servuices section for lg screens

--- app/components/offered_services/Offered_services.tsx
+++ app/components/offered_services/Offered_services.tsx
@@ -3,7 +3,7 @@ import Services_Grid from "./Services_Grid";
 export default function Offered_services() {
   return (
     <div className="offered_services">
-      <h2 className="text-2xl text-blue-800 font-bold text-center mt-4">Serviços Ofertados</h2>
+      <h2 className="text-2xl md:text-3xl lg:text-4xl text-blue-800 font-bold text-center mt-4 ">Serviços Ofertados</h2>
       <Services_Grid />
     </div>
   );
