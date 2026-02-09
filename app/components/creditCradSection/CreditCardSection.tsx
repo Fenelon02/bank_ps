bc449dd0f0688f805dccd350c90dcbfc1648d38d
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-09 16:33:33 -0300

    FEAT: remove the button from the credit card section

--- app/components/creditCradSection/CreditCardSection.tsx
+++ app/components/creditCradSection/CreditCardSection.tsx
@@ -7,7 +7,6 @@ export default function CreditCardSection() {
             <h2 className="text-white text-3xl md:text-5xl font-bold mb-2 text-center mt-4">Aproveite e decole no <span className="text-green-500">Brasil</span> e no <span className="text-yellow-500">Mundo</span> com um só <span className="">Cartão</span></h2>
             <p className="text-gray-300 mb-2 text-center text-lg md:text-2xl">Desfrute de vantagens exclusivas com nosso cartão internacional.</p>
             <CreditCardCarousel />
-            <Button additionalClass="mx-auto m-4 block" variant="secondary" size="large">Solicitar Cartão</Button>
         </div>
     )
 }
