a71c62ebef479f8edfce3dfdf5943175afd5c6f8
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-10 08:18:17 -0300

    CHORE: add an id on the services section

--- app/components/offered_services/Offered_services.tsx
+++ app/components/offered_services/Offered_services.tsx
@@ -2,7 +2,7 @@ import Services_Grid from "./Services_Grid";
 
 export default function Offered_services() {
   return (
-    <div className="offered_services">
+    <div id="Services">
       <h2 className="text-2xl md:text-3xl lg:text-4xl text-blue-800 font-bold text-center mt-4 ">Serviços Ofertados</h2>
       <Services_Grid />
     </div>
