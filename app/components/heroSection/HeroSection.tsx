4f0231c8ae10732930b48689796de758e0755d16
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-10 08:16:21 -0300

    FEAT: add an id on the hero section

--- app/components/heroSection/HeroSection.tsx
+++ app/components/heroSection/HeroSection.tsx
@@ -2,7 +2,7 @@ import Button from "../ui/button/Button";
 
 export default function HeroSection() {
     return(
-        <div className="bg-[url(@/public/images/HeroBg.jpg)] bg-cover bg-center h-screen w-full flex items-end md:items-start">
+        <div id="Home" className="bg-[url(@/public/images/HeroBg.jpg)] bg-cover bg-center h-screen w-full flex items-end md:items-start">
             <section className="font-sans text-white font-bold flex flex-col p-7 md:grid md:grid-rows-2 md:h-full">
                 <div className="md:flex md:items-start md:flex-col md:mt-20 gap-2">
                     <h2 className="text-3xl md:text-5xl lg:text-6xl">Seu Banco em sua mão</h2>
