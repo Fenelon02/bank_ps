ba0ff6678d4a048944735f99e6c01c4b7b8b4901
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-13 14:13:03 -0300

    FEAT: fix the padding on the offered services

--- app/components/offered_services/Offered_services.tsx
+++ app/components/offered_services/Offered_services.tsx
@@ -2,7 +2,7 @@ import Services_Grid from "./Services_Grid";
 
 export default function Offered_services() {
   return (
-    <div id="Services">
+    <div id="Services" className="px-8 md:px-12 lg:px-20">
       <h2 className="text-2xl md:text-3xl lg:text-4xl text-blue-800 font-bold text-center mt-4 ">Serviços Ofertados</h2>
       <Services_Grid />
     </div>
