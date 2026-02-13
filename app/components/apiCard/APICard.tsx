b479dbc99d6847b40699fa34c720617501cb14a0
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-13 14:08:23 -0300

    FEAT: fix the padding and fix the flex box on the api card

--- app/components/apiCard/APICard.tsx
+++ app/components/apiCard/APICard.tsx
@@ -32,9 +32,9 @@ const logos = [
 
 export default function APICard() {
   return(
-    <div className="m-4 bg-blue-900 p-6 rounded-lg text-white grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-4 items-center">
+    <div className="m-4 bg-blue-900 p-6 mx-12 md:mx-14 lg:mx-22 rounded-lg text-white grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-4 items-center">
         <h2 className="text-3xl font-bold mb-4 md:text-4xl text-center lg:text-5xl">API de fácil integração a <br />diferentes sistemas</h2>
-       <div className="flex justify-center lg:flex-wrap">
+       <div className="flex justify-center flex-wrap">
         {logos.map((logo, index) => 
           (
             <img key={index} src={logo.logoUrl} alt={logo.name} className="h-12  md:h-25 md:w-25 lg:h-30 w-12 lg:w-30 m-2 inline-block" />
